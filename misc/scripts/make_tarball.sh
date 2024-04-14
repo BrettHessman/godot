310d5b333292e1bca758badfdecf8040e7860ffc
Brett Hessman <bhessman37@gmail.com>
2024-04-14 16:53:30 -0600

    fix name

--- misc/scripts/make_tarball.sh
+++ misc/scripts/make_tarball.sh
@@ -52,7 +52,7 @@ echo
 echo "The tarball will be written to the parent folder:"
 echo "    $(dirname $CURDIR)/$NAME.tar.gz"
 
-git-archive-all $HEAD --prefix=$NAME/ -o $TMPDIR/$NAME.tar
+git-archive-all $HEAD --prefix=$NAME/ $TMPDIR/$NAME.tar
 
 # Adding custom .git/HEAD to tarball so that we can generate VERSION_HASH.
 cd $TMPDIR
